a4e6643a180c503cfac3861c0fd1cbf636c32379
vibaviattigala <vibaviattigala@gmail.com>
2017-06-26 15:10:32 +0530

    Read source character by character

--- compiler.c
+++ compiler.c
@@ -1,9 +1,41 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+#include <fcntl.h> 
 
-int main()
+int main(int argc, char *argv[])
 {
     
-    
+    if ( argc != 2 ) 
+    {
+       
+        printf( "usage: %s filename\n", argv[0] );
+    }
+    else 
+    {
+        
+        int fd=open(argv[1], O_RDONLY, 0);
+
+        
+        if ( fd == 0 )
+        {
+            printf( "Could not open file\n" );
+        }
+        else 
+        {
+            int readb=0,lineno=0;
+            do{
+            char c;
+            readb=read(fd,&c,1);
+            if(c=='\n')
+            {
+                lineno++;
+            }
+            printf("%c",c);
+            }while(readb!=0);
+            printf("Total Line Numbers %d\n",lineno);
+        }
+    }
     
     
     
---
+++ hello.m
@@ -0,0 +1,19 @@
+Start{
+    
+        int a=10;
+        float c=1;
+        for(int b=0;b<5;b++)
+        {
+            
+            a=a+c;
+         
+            
+            
+        }
+    
+    
+    
+    
+    
+    
+}
